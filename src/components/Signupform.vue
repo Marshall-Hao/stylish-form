cc2ede94fd3b6aac1b841366dd1cbace61a90f33
marshall chen <marshallchan666@outlook.com>
2021-06-11 17:10:26 +0800

    checkbox

--- src/components/Signupform.vue
+++ src/components/Signupform.vue
@@ -1,6 +1,34 @@
 <template>
     <form class="justify-self-center px-20 py-10 bg-white w-3/5 mx-auto my-10 space-y-7 rounded-xl shadow-lg">
         <label class="block text-gray-400 text-sm tracking-wider font-semibold">Email:</label>
-        <input class="block border-b w-full text-gray-400 text-sm tracking-wider font-medium focus:outline-none" type="email" required>
+        <input v-model="email" class="block border-b w-full text-gray-400 text-sm tracking-wider font-medium focus:outline-none" type="email" required>
+
+        <label class="block text-gray-400 text-sm tracking-wider font-semibold">Password:</label>
+        <input v-model="password" class="block border-b w-full text-gray-400 text-sm tracking-wider font-medium focus:outline-none" type="password" required>
+
+        <label class="block text-gray-400 text-sm tracking-wider font-semibold">Role:</label>
+        <select v-model="role" class="block border-b w-full text-gray-400 text-sm tracking-wider font-medium focus:outline-none">
+            <option value="developer">Web Developer</option>
+            <option value="designer">Web Designer</option>
+        </select>
+
+        <div class="block space-x-4 text-gray-400 text-sm tracking-wider font-semibold flex items-center">
+            <input v-model="terms" class="inline-block form-checkbox" type="checkbox" required>
+            <label class="inline-block ">Accept terms and condition</label>
+        </div>
     </form>
+    {{terms}}
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: '',
+            terms: false
+        }
+    }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      outline: {
+        gray: '2px solid #F3F4F6',
+      }
+    },
   },
   variants: {
     extend: {},
