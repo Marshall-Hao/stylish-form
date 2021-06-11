5e87ad5bfb5a9f91d89072cefd969c6c4a44a621
marshall chen <marshallchan666@outlook.com>
2021-06-11 15:49:17 +0800

    set up signup form component

---
+++ src/components/Signupform.vue
@@ -0,0 +1,6 @@
+<template>
+    <form class="justify-self-center px-20 py-10 bg-white w-3/5 mx-auto my-10 space-y-7 rounded-xl shadow-lg">
+        <label class="block text-gray-400 text-sm tracking-wider font-semibold">Email:</label>
+        <input class="block border-b w-full text-gray-400 text-sm tracking-wider font-medium focus:outline-none" type="email" required>
+    </form>
+</template>
